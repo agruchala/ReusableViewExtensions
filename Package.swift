935ceff606b16a46258730786caed91a5f2aec24
Artur Gruchała <gruchala.a@gmail.com>
2020-04-12 15:12:10 +0200

    Add Swift package manager suppport

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "ReusableViewExtensions",
-    platforms: [ .iOS(.v9),
-                 .tvOS(.v9),
-    ],
     products: [
         .library(
             name: "ReusableViewExtensions", targets: ["ReusableViewExtensions"])
